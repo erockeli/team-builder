c8dfb0db1cb32e5b31f29b0e915bd18dffa9ba18
Eric Foster <elitheroyalone@gmail.comgit config --global user.name eric-fostergit config --global user.email elitheroyalone@gmail.com>
2019-10-16 17:08:32 -0500

    eric-foster

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -1,3 +1,42 @@
-// Name
-// Role
-// 
+import React, { useState } from "react";
+
+const Form = props => {
+  // console.log("this is our props",props);
+  const [note, setNote] = useState({
+    title: "",
+    body: ""
+  });
+  const changeHandler = event => {
+    setNote({ ...note, [event.target.name]: event.target.value });
+    console.log(event.target.value);
+  };
+  const submitForm = event => {
+    event.preventDefault();
+    props.addNewNote(note);
+    setNote({ title: "", body: "" });
+  };
+  return (
+    <form onSubmit={submitForm}>
+      <label htmlFor="title">Title</label>
+      <input
+        name="title"
+        id="title"
+        type="text"
+        placeholder="title"
+        onChange={changeHandler}
+        value={note.title}
+      />
+      <label htmlFor="body">Description</label>
+      <textarea
+        name="body"
+        id="body"
+        placeHolder="Type your note here"
+        onChange={changeHandler}
+        value={note.body}
+      />
+      <button type="submit">Add Note</button>
+    </form>
+  );
+};
+
+export default NoteForm;

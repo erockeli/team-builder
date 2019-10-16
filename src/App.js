0b30ef631e29bee4dc97f55fda6efef9ec028212
Eric Foster <elitheroyalone@gmail.comgit config --global user.name eric-fostergit config --global user.email elitheroyalone@gmail.com>
2019-10-16 16:55:59 -0500

    Establishing my state in app.js

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    const [teamMember, setTeamMember]= useState([{
+
+      name: 'Elijah Foster',
+      email: 'eliWoods@email.com',
+      role: 'UX Designer'
+
+    }])
+
+    const addNewTeamMember = teamMember => {
+      setNotes([...teamMembers, teamMember])
+    }
+}
+
+export default App;
